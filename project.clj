6ce000c21b4e280cc0718e210214afb65359df21
KingCode <jrompre@gmail.com>
2013-07-08 17:26:44 -0400

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cursely-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[robert/hooke "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]])
