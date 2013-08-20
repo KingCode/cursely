774f76b467bd60871ddf67a9aa1bb742ce1c2886
KingCode <jrompre@gmail.com>
2013-08-20 00:21:07 -0400

    core skeletton and low-level: parse-markup, strip fns

--- src/recursely/core.clj
+++ src/recursely/core.clj
@@ -1,5 +1,6 @@
 (ns recursely.core
   (:use recursely.ccore 
+        [clojure [walk]]
         [clj-utils [core :only [thread-it]]]))
 
 (comment "(Not implemented) API which transforms a recursive function into a non-recursive one, e.g. 
@@ -37,6 +38,74 @@ where each func spec is a name-args-body
 "
 )
 
+
+(defn strip
+[ form ]
+  (let [ ftok (apply str (-> (first form) str rest)) ]
+      `( ~(symbol ftok) ~@(rest form))))
+
+
+(defn parse-markup
+"Strips markup from function positions in forms within body, storing transformed
+ forms as keys in a may, and yields a tuple of the new markup in pos. 0, and the 
+ map."
+[ body ]
+  (let [ regs (atom {})
+         update (fn [form value] (swap! regs #(merge % {form value}))) ]
+           [ (prewalk #(if (and (list? %) 
+                       (-> (first %) str (.startsWith "$")))
+                                (let [ stripped (strip %) ]
+                                            (update stripped {}) stripped) 
+                                                %) body)
+              ,@regs ]))
+
+
+#_(defn adapt-1
+"Yields an adapted form from a top-level marked up form."
+[ form ]
+  (condp))
+
+  
+
+
+(defn- parsefor-numargs
+"Yields a map of each list form in body with the number of args for its var 
+ in function position."
+[ regs body ]
+(let [ form2mumargs (atom {}) ]
+  #_(prewalk #(do (if (list? %) (swap! form2numargs 
+                                    (fn [x] (assoc x % (dec (count %)))))) %)
+                                                                            body)
+  (postwalk (fn [ x ] (if (list? x) 
+                          (condp instance? (first x)))))))
+  
+
+
+(defn- adapt-1
+[ regs name args-body ]
+    (let [ body (drop 1 args-body) ]))
+       
+
+;;args, body copied from clojure.core_deftypes
+(defn- parse-specs
+[specs]
+  (loop [ret {} s specs]
+      (if (seq s)
+            (recur (assoc ret (first s) (take-while seq? (next s)))
+                         (drop-while seq? (next s)))
+                               ret)))
+
+
+;;body copied from clojure.core_deftypes
+#_(defn- emit-adapted [specs]
+  (let [impls (parse-impls specs)
+        registered (set (keys impls)) ]
+      `(do
+             ~@(map (fn [[name arities]]
+                             `(adapt registered ~name ~@arities ))
+                                           impls))))
+
+
 (defn hoist-spec
 "Yields a map of symbols from form to maps of {:type <hfn, hcall, nil> :numargs <positive int> :val}
  for all symbols in form requiring hoisting by recursely.
@@ -51,7 +120,7 @@ where each func spec is a name-args-body
  )
 
 (defmacro defrecursely
-"Creates a recursely adapted function for each spec, where spec is a (name ([& args] body)*)."
+"Creates a recurseldeclared functiony adapted function for each spec, where spec is a (name ([& args] body)*)."
 [ spec & specs ] )
 
   
