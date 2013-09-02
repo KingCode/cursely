61c8d8945d99b266534270ad1b074597c6e06aca
KingCode <jrompre@gmail.com>
2013-09-01 20:25:59 -0400

    core: added tests for all non-variadic transforms

--- test/recursely/core_test.clj
+++ test/recursely/core_test.clj
@@ -7,13 +7,66 @@
         clojure.test
         [robert.hooke :as h]))
                 
-(def ^:dynamic  KS) 
 
-(deftest transform-test
-    (testing "A form nesting calls to KS should yield the correct framework adapted form"
-    (binding [KS (fn []) ] ;;dummy function for keep compiler quiet for now
+(deftest transform-test-counter
+  (testing "Simplest, recursive counter form should yield correct adapted form"
+     (let [ src '(+ 1 (counter (rest coll)))
+            exp '(-> (recursely.ccore/hparam [stack pos] 1)
+                     (recursely.ccore/hparam (rest coll))
+                     (recursely.ccore/hcall (fn [arg1 arg2 arg3] (counter arg1 arg2 arg3)) 1)
+                     (recursely.ccore/hfn (fn [arg1 arg2] (+ arg1 arg2)) 2)
+                     (recursely.ccore/rewind pos)) 
+            act (transform src #{'counter}) ]
+        (is (= exp act)))))
+
+
+(deftest transform-test-odd?
+  (testing "Simplest recursive odd? form should yield correct adapted form"
+    (let [ src '(recursive-odd? (- x 2))
+           exp '(-> (recursely.ccore/hparam [stack pos] (- x 2))
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3] (recursive-odd? arg1 arg2 arg3)) 1)
+                    (recursely.ccore/rewind pos))
+           act (transform src #{'recursive-odd?}) ]
+        (is (= exp act)))))
+
+
+(deftest transform-test-cat
+  (testing "Simple fn cat form should yield correct adapted form"
+    (let [ src '(hat (dec n) (+ term acc))
+           exp '(-> (recursely.ccore/hparam [stack pos] (dec n))
+                    (recursely.ccore/hparam (+ term acc))
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3 arg4] (hat arg1 arg2 arg3 arg4)) 2)
+                    (recursely.ccore/rewind pos))
+           act (transform src #{'hat 'cat}) ]
+        (is (= exp act)))))
+
+
+(deftest transform-test-hoffstadter
+  (testing "Male/female sub-form should yield correct adapted form"
+    (let [ src '(- n (female (male (dec n))))
+           exp '(-> (recursely.ccore/hparam [stack pos] n)
+                    (recursely.ccore/hparam (dec n))
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3] (male arg1 arg2 arg3)) 1)
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3] (female arg1 arg2 arg3)) 1)
+                    (recursely.ccore/hfn (fn [arg1 arg2] (- arg1 arg2)) 2)
+                    (recursely.ccore/rewind pos))
+           act (transform src #{'male 'female}) ]
+        (is (= exp act)))))
+
+
+(deftest transform-test-KS-1
+  (testing "Simple form from KS logic should yield correct framework adapted form"
+    (let [ src '(KS tail capacity) 
+           exp '(-> (recursely.ccore/hparam [stack pos] tail)
+                    (recursely.ccore/hparam capacity)
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3 arg4] (KS arg1 arg2 arg3 arg4)) 2)
+                    (recursely.ccore/rewind pos))
+           act (transform src #{'KS}) ]
+        (is (= exp act))))) 
+
+(deftest transform-test-KS-2
+  (testing "A form nesting calls to KS should yield the correct framework adapted form"
         (let [ 
-
                 src '(max (KS tail capacity)  (+ v (KS tail (- capacity c))))
 
                 exp '(-> (recursely.ccore/hparam [stack pos] tail)
@@ -35,5 +88,32 @@
     (println "ACTUAL: " ) 
         (foreach [ f act] (println f)))
 
-          (is (= exp act)))))) 
+          (is (= exp act))))) 
 
+
+(deftest transform-test-powercounter
+  (testing "A complex form nesting multiple and mutually recursive  calls and fns mixed with multiple 
+            positional parameters, should yield the correct adapted form"
+    (let [ src '(power-counter (rest power-in-numbers1) 
+                      (* factor (+ (counter power-in-numbers1)
+                                   (counter power-in-numbers2))) 
+                      (rest power-in-numbers2)  
+                      (rest coll))
+
+           exp '(-> (recursely.ccore/hparam [stack pos] (rest power-in-numbers1))
+                    (recursely.ccore/hparam factor)
+                    (recursely.ccore/hparam power-in-numbers1)
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3] (counter arg1 arg2 arg3)) 1)
+                    (recursely.ccore/hparam power-in-numbers2)
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3] (counter arg1 arg2 arg3)) 1)
+                    (recursely.ccore/hfn (fn [arg1 arg2] (+ arg1 arg2)) 2) 
+                    (recursely.ccore/hfn (fn [arg1 arg2] (* arg1 arg2)) 2)
+                    (recursely.ccore/hparam (rest power-in-numbers2))
+                    (recursely.ccore/hparam (rest coll))
+                    (recursely.ccore/hcall (fn [arg1 arg2 arg3 arg4 arg5 arg6]
+                                                (power-counter arg1 arg2 arg3 arg4 arg5 arg6)) 4)
+                    (recursely.ccore/rewind pos))
+
+            act (transform src #{'power-counter 'counter}) ]
+
+        (is (= exp act)))))
